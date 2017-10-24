c579562d41d5304d61bc4451b128a637d686eb0f
Neil Aquilina <neila@mcast.org.mt>
2017-10-24 14:42:34 +0200

    Finished NUmberWizardUI

---
+++ NumberWizardUI/NumberWizardUI/Assets/scripts/NumberWizard.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class NumberWizard : MonoBehaviour {
+
+    //creates 4 number variables
+    int min = 1;
+    int max = 1000;
+    int guess = 500;
+    int maxNumberOfGuesses = 10;
+
+    public Text guessText;
+
+    //the number is higher than the one guessed
+    public void GuessHigher()
+    {
+        min = guess;
+        NextGuess();
+    }
+    
+    //the number is smaller than the one guessed
+    public void GuessLower()
+    {
+        max = guess;
+        NextGuess();
+    }
+
+    void NextGuess()
+    {
+        guess = (max + min) / 2;
+        //update the Text with the new guess
+        guessText.text = guess.ToString();
+        maxNumberOfGuesses--;
+        //if all tries are used
+        if (maxNumberOfGuesses == 0)
+        {
+            SceneManager.LoadScene("Win");
+        }
+    }
+
+    void StartGame()
+    {
+        //prints once to the Console
+        print("Welcome to Number Wizard");
+        Debug.Log("Please choose a number in your head between " + min + " and " + max);
+        //calling method NextGuess()
+        NextGuess();
+    }
+
+    // Use this for initialization
+    void Start () {
+
+        StartGame();
+        
+        	
+	}
+	
+	
+}
