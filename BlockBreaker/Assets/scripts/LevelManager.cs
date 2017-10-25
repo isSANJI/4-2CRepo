c9c98d0748218fd60aa4c6251bf2dd0b54c4e445
Neil Aquilina <neila@mcast.org.mt>
2017-10-25 11:57:41 +0200

    Added BlockBreaker with background music

---
+++ BlockBreaker/Assets/scripts/LevelManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+    public void LoadLevel(string levelName)
+    {
+        Debug.Log("Level: " + levelName);
+        //loads the scene named levelName
+        SceneManager.LoadScene(levelName);
+    }
+
+    public void QuitGame()
+    {
+        UnityEditor.EditorApplication.isPlaying = false;
+    }
+
+	
+	
+	
+
+}
---
+++ BlockBreaker/Assets/scripts/MusicPlayer.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour {
+
+    //static variables or objects are available throughout 
+    //all instances of the same class
+    static MusicPlayer myMusicPlayer = null;
+
+    //Awake() is the first method run
+    // so we are destroying the object as soon as 
+    // the program runs
+    void Awake()
+    {
+
+        //if an instance of myMusicPlayer exists
+        if (myMusicPlayer != null)
+        {
+            Debug.Log("Destroying object " + this.gameObject.GetInstanceID().ToString());
+            //destroy the new object created
+            Destroy(this.gameObject);
+        }
+        else //myMusicPlayer = null
+        {
+            //myMusicPlayer is filled with my FIRST Sound Object
+            myMusicPlayer = this;
+            //this.gameObject = the gameObject attached to 
+            //this script, being SoundObject
+            //DontDestroyOnLoad = do not destroy the gameObject
+            //when loading a new scene
+            GameObject.DontDestroyOnLoad(this.gameObject);
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
