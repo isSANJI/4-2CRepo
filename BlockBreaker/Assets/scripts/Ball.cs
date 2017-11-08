fbd0493fcdedde699cadae03d3b8afa71b698bf0
Neil Aquilina <neila@mcast.org.mt>
2017-11-08 12:02:16 +0100

    Updated BlockBreaker with Colliders, RigidBody, Bounciness and start on Click

---
+++ BlockBreaker/Assets/scripts/Ball.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+    public Paddle myPaddle;
+
+    private Vector3 paddleToBallVector;
+
+    private bool hasStarted = false;
+
+	// Use this for initialization
+	void Start () {
+
+        //saves the distance between the ball and the paddle
+        paddleToBallVector = this.transform.position - myPaddle.transform.position;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (!hasStarted) //if hasStarted == false
+        {
+            //attaches the ball to the paddle
+            //keeping the distance
+            this.transform.position = myPaddle.transform.position + paddleToBallVector;
+
+            
+            //if left click is pressed
+            if (Input.GetMouseButtonDown(0))
+            {
+                hasStarted = true;
+                Debug.Log(hasStarted);
+                this.GetComponent<Rigidbody2D>().velocity = new Vector2(2f, 10f);
+                
+            }
+        }
+
+        
+		
+	}
+}
---
+++ BlockBreaker/Assets/scripts/Paddle.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //saves the mousePosition in Units
+        float mousePosInUnits = (Input.mousePosition.x / Screen.width * 13f) - 6.5f;
+
+        //restricts mousePosInUnits between -6f and 6f
+        float mousePos = Mathf.Clamp(mousePosInUnits, -6f, 6f);
+
+        //Vector3: saves any coordinate in 3D space
+        //as x,y,z
+        //this.transform.position.y = keeps the original y-axis
+        Vector3 paddlePosition = new Vector3(mousePos, this.transform.position.y, 0);
+
+        this.transform.position = paddlePosition;
+		
+	}
+}
